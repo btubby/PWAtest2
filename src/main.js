9efa3300597cb86d68772a29bd551078a225b887
Ben Tubby <BenTubby@paypoint.com>
2022-01-27 18:40:24 +0000

    more

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './components/App';
+
+import './global.css';
+import './skeletons.css';
+import './registerServiceWorker'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
