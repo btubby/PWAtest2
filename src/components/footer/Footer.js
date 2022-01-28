4806e1df7e4e6c603167509ad3d2fdf4c1788767
Ben Tubby <BenTubby@paypoint.com>
2022-01-28 10:32:11 +0000

    more

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,11 +3,16 @@ import { Link } from 'react-router-dom';
 
 import Camera from 'react-feather/dist/icons/camera';
 import Clock from 'react-feather/dist/icons/clock';
+import Tri from 'react-feather/dist/icons/triangle';
 
 import './footer.css';
 
 const Footer = () => (
   <div className="footer">
+     <Link className="footer__link" to="/login" >
+      <Tri />
+      Login
+    </Link>
     <Link className="footer__link" to="/" >
       <Camera />
       Scan
